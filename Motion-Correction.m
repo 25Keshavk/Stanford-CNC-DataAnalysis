820ece6c19f033821c8b2ac5893e70b436a27f76
25Keshavk <110709267+25Keshavk@users.noreply.github.com>
2022-11-27 19:20:48 -0800

    Add files via upload

---
+++ Motion-Correction.m
@@ -0,0 +1,97 @@
+% Perform motion correction on a given input. 
+%
+% This program uses the NoRMCorre package developed by Pnevmatikakis and
+% Giovannucci. It can be found at
+% https://github.com/flatironinstitute/NoRMCorre. 
+%
+% @author Pnevmatikakis, Eftychios A
+% @author Giovannucci, Andrea
+% 
+% @author Keshav Kotamraju 
+
+
+clear
+gcp;
+
+name = 'full-task-3.tif';
+
+tic; Y = read_file(name); toc; % read the file (optional, you can also pass the path in the function instead of Y)
+Y = single(Y);                 % convert to single precision 
+T = size(Y,ndims(Y));
+Y = Y - min(Y(:));
+
+%% set parameters (first try out rigid motion correction)
+
+options_rigid = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),'bin_width',200,'max_shift',15,'us_fac',50,'init_batch',200);
+
+%% perform motion correction
+tic; [M1,shifts1,template1,options_rigid] = normcorre(Y,options_rigid); toc
+
+%% now try non-rigid motion correction (also in parallel)
+options_nonrigid = NoRMCorreSetParms('d1',size(Y,1),'d2',size(Y,2),'grid_size',[32,32],'mot_uf',4,'bin_width',200,'max_shift',15,'max_dev',3,'us_fac',50,'init_batch',200);
+tic; [M2,shifts2,template2,options_nonrigid] = normcorre_batch(Y,options_nonrigid); toc
+
+%% compute metrics
+
+nnY = quantile(Y(:),0.005);
+mmY = quantile(Y(:),0.995);
+
+[cY,mY,vY] = motion_metrics(Y,10);
+[cM1,mM1,vM1] = motion_metrics(M1,10);
+[cM2,mM2,vM2] = motion_metrics(M2,10);
+T = length(cY);
+
+%% plot metrics
+figure;
+    ax1 = subplot(2,3,1); imagesc(mY,[nnY,mmY]);  axis equal; axis tight; axis off; title('mean raw data','fontsize',14,'fontweight','bold')
+    ax2 = subplot(2,3,2); imagesc(mM1,[nnY,mmY]);  axis equal; axis tight; axis off; title('mean rigid corrected','fontsize',14,'fontweight','bold')
+    ax3 = subplot(2,3,3); imagesc(mM2,[nnY,mmY]); axis equal; axis tight; axis off; title('mean non-rigid corrected','fontsize',14,'fontweight','bold')
+    subplot(2,3,4); plot(1:T,cY,1:T,cM1,1:T,cM2); legend('raw data','rigid','non-rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
+    subplot(2,3,5); scatter(cY,cM1); hold on; plot([0.9*min(cY),1.05*max(cM1)],[0.9*min(cY),1.05*max(cM1)],'--r'); axis square;
+        xlabel('raw data','fontsize',14,'fontweight','bold'); ylabel('rigid corrected','fontsize',14,'fontweight','bold');
+    subplot(2,3,6); scatter(cM1,cM2); hold on; plot([0.9*min(cY),1.05*max(cM1)],[0.9*min(cY),1.05*max(cM1)],'--r'); axis square;
+        xlabel('rigid corrected','fontsize',14,'fontweight','bold'); ylabel('non-rigid corrected','fontsize',14,'fontweight','bold');
+    linkaxes([ax1,ax2,ax3],'xy')
+%% plot shifts        
+
+shifts_r = squeeze(cat(3,shifts1(:).shifts));
+shifts_nr = cat(ndims(shifts2(1).shifts)+1,shifts2(:).shifts);
+shifts_nr = reshape(shifts_nr,[],ndims(Y)-1,T);
+shifts_x = squeeze(shifts_nr(:,1,:))';
+shifts_y = squeeze(shifts_nr(:,2,:))';
+
+patch_id = 1:size(shifts_x,2);
+str = strtrim(cellstr(int2str(patch_id.')));
+str = cellfun(@(x) ['patch # ',x],str,'un',0);
+
+figure;
+    ax1 = subplot(311); plot(1:T,cY,1:T,cM1,1:T,cM2); legend('raw data','rigid','non-rigid'); title('correlation coefficients','fontsize',14,'fontweight','bold')
+            set(gca,'Xtick',[])
+    ax2 = subplot(312); plot(shifts_x); hold on; plot(shifts_r(:,1),'--k','linewidth',2); title('displacements along x','fontsize',14,'fontweight','bold')
+            set(gca,'Xtick',[])
+    ax3 = subplot(313); plot(shifts_y); hold on; plot(shifts_r(:,2),'--k','linewidth',2); title('displacements along y','fontsize',14,'fontweight','bold')
+            xlabel('timestep','fontsize',14,'fontweight','bold')
+    linkaxes([ax1,ax2,ax3],'x')
+
+
+% Save each of the frames in a tiff file
+
+for t = 1:1:T
+    
+    f = figure('visi ble','off'); % making it so plots won't be shown
+
+    subplot(131);
+    imagesc(M1(:,:,t),[nnY,mmY])
+
+    axis equal;
+    axis tight;
+
+    set(gca,'XTick',[])
+    set(gca,'YTick',[])
+    set(gca,'Position',[0 0 1 1]) % Make the axes occupy the whole figure
+
+
+    colormap('bone')
+
+    saveas(gcf, strcat('results/corrected-frame-', string(t)),'tif') % gc means save current figure
+end 
